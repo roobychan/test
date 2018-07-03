8dec07b878a5c9922f6c969fc07a2d739e5ee68a
Chen <roochen@apac.adp.com>
2018-07-03 12:26:49 +0800

    modified

--- TaskNotify.py
+++ TaskNotify.py
@@ -1,7 +1,8 @@
 import re
 
 
-USStatus = {1:"Under Analysis/Devlopment",2:"Waiting for Documentation",3:"Waiting for testing",4:"Completed"}
+USStatus = {1: "Under Analysis/Devlopment", 2: "Waiting for Documentation", 3: "Waiting for testing", 4: "Completed"}
+
 
 class Task:
     taskID = ""
@@ -17,13 +18,13 @@ class Task:
         self.status = dline[5]
 
 class UserStory:
-    task[Task] = []
+    tasks = [Task]
     userStory = ""
     CRM = ""
     description = ""
     status = "4"
     def __init__(self, dline=[]):
-        match = re.match(".+?(?=:)",dline[2])
+        match = re.match("(?<=US).+?(?=:)",dline[2])
         if match:
             self.userStory = match.group(0)
         match = re.match("(?<=:\W).*\S+-?(?=-)",dline[2])
@@ -41,21 +42,53 @@ class UserStory:
             if match:
                 self.CRM = match.group(0)
         self.owner = dline[10]
+        self.addTask(dline)
 
     def updateStatus(self):
-        for t in self.task:
-            if (re.match("Coding",t.description) or re.match("Analy", t.description)) and t.status != "Completed"
+        for t in self.tasks:
+            if (re.match("Coding",t.taskType) or re.match("Analy", t.taskType)) and t.status != "Completed":
                 self.status = USStatus[1]
                 break
-            elif re.match("Fucntional test", t.description) and t.status != "Completed"
-                self.status = USstatus[3]
+            elif re.match("Fucntional test", t.taskType) and t.status != "Completed":
+                self.status = USStatus[3]
                 break
         if self.status != 1:
-            for t in self.task:
-                if t.status != 'Completed'
-                self.status = USStatus[2]
+            for t in self.tasks:
+                if t.status != 'Completed':
+                    self.status = USStatus[2]
+    
+    def addTask(self, dline=[]):
+        self.tasks.append(Task(dline))
+    
 
 
+def main():
+    readData = []
+    UStories = [UserStory]
+    with open('export.csv') as f:
+        for l in f:
+            readData.append(l)
+    readData.pop(0)
+
+    for r in readData:
+        slist = r.split(",")
+        match = re.match("(?<=US).+?(?=:)", slist[2])
+        if match:
+            new = True
+            for u in UStories:
+                if match.group(0) == u.userStory:
+                    new = False
+                    u.addTask(u,slist)
+                    break
+            if new:
+                UStories.append(UserStory(slist))
+
+    for u in UStories:
+        print(u.userStory, u.description, u.CRM,u.status)
+        for t in u.tasks:
+            print(t.taskID,t.taskType,t.status)
+
+main()
             
 
     
--- python.md
+++ python.md
@@ -32,3 +32,12 @@ float()
 #Returns String
 a = input("input")
 ```
+
++ Regex
+
+```python
+import re
+match = re.match("pattern",str)
+if match:
+    result = match.group(0)
+```
